a06a2457d518ac20e340c922edd40e27283d41f3
twflu <yzzhou@thoughtworks.com>
2014-07-08 14:25:18 +0800

    Initial Commit

---
+++ stdfunctionLearn/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  stdfunctionLearn
+//
+//  Created by twer on 7/8/14.
+//  Copyright (c) 2014 twer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
